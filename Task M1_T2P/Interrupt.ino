8e9cc28fcb6f95fc2813b2d3f603e34868e7368c
Sidhant4843 <131806438+Sidhant4843@users.noreply.github.com>
2024-01-31 23:47:54 +0530

    Create interrupt.ino

---
+++ Task M1_T2P/Interrupt.ino
@@ -0,0 +1,24 @@
+#define SENSOR_PIN 2
+#define ACTUATOR_PIN 6
+volatile boolean sensorState = false;
+// Volatile to ensure visibility in ISR
+void setup () {
+pinMode (SENSOR_PIN, INPUT) ; pinMode (ACTUATOR_PIN, OUTPUT) ;
+Serial. begin (9600);
+attachInterrupt (digitalPinToInterrupt(SENSOR_PIN), sensorISR, CHANGE) ;
+// Attach interrupt
+}
+void loop () {
+// No need for polling or delays, interrupts handle sensor changes
+}
+void
+sensorISR() {
+sensorState = digitalRead (SENSOR_PIN) ;
+if (sensorState == HIGH) {
+Serial println ( "HIGH");
+digitalwrite(ACTUATOR_PIN, HIGH) ;
+else｛
+Serial println ("LOW");
+digitalWrite(ACTUATOR_PIN, LOW);
+}
+}
